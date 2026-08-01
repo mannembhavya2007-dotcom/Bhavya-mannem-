d5db6fe55aaf6b780d921fda465a782cc804e60a
mannembhavya2007-dotcom <mannembhavya2007@gmail.com>
2026-08-01 09:48:17 +0530

    Add files via upload

---
+++ Hello world.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+        System.out.println("My name is Bhavya.");
+        System.out.println("This is my first GitHub repository.");
+    }
+}
